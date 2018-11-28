c1f4494cc3f2c79dc2002cd432e5bcd784784016
SomaiyahNazari <45323597+SomaiyaNazari@users.noreply.github.com>
2018-11-28 22:53:34 +0800

    Add files via upload
    Assignment 3 Mobile application section 2

---
+++ App.js
@@ -0,0 +1,64 @@
+
+//task: Reza
+
+import React, {Component} from 'react';
+import {Text, View, Button, TextInput, Alert} from 'react-native';
+import {Styles} from './Styles';
+
+export default class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      word:"detect",
+      Consonants:0,
+      Vowel:0,
+      Characters:0
+
+    }
+  }
+
+//task: Habiba
+  AnalyzeWord = () => {
+    let ccount=0,cvowel=0,no_of_char=0;
+    let string=this.state.word;
+    let str= string.split(' ',string.length);
+    let word=this.state.word;
+    
+    no_of_char=word.length;
+    for(var i=0;i<word.length;i++){
+      let lchar=word[i].toLowerCase();
+      if(lchar=='a' || lchar=='e' || lchar=='i' || lchar=='o' || lchar=='u')
+        cvowel++;
+      else
+        ccount++;
+    }
+    this.setState({Consonants:ccount,Vowel:cvowel,Characters:no_of_char});
+
+    
+  }
+
+//task: Somiyah
+  render() {
+    return (
+      <View style={Styles.container}>
+        <View style={Styles.container}>
+        <Text style={Styles.welcome}>A Word Analyzer</Text>
+       
+        <View style={Styles.buttons}>
+ <Text style={Styles.buttons}>Word  </Text>
+ 
+ <TextInput onChangeText={(word) => this.setState({word})} style={Styles.instructions} placeholder='detect'/>
+        
+         <Button color="#841584" onPress={this.AnalyzeWord} title='Analyze'/>
+        
+        </View>
+        <Text style={Styles.oscar}></Text>
+        <Text style={Styles.oscar}>Word                                         : {this.state.word}</Text>
+        <Text style={Styles.oscar}>No of Consonants                   : {this.state.Consonants}</Text>
+        <Text style={Styles.oscar}>No of Vowels                           : {this.state.Vowel}</Text>
+        <Text style={Styles.oscar}>No of Characters                     : {this.state.Characters}</Text>
+        </View>
+      </View>
+    );
+  }
+}
---
+++ Styles.js
@@ -0,0 +1,37 @@
+import {StyleSheet} from 'react-native';
+
+export const Styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      marginLeft: 10,
+      color:'blue'
+    },
+    oscar: {
+
+fontSize:14,
+margin:10,
+marginLeft:50
+    },
+margary:{
+  marginLeft:100
+  
+},
+
+    buttons: {
+      
+    justifyContent : 'flex-end',
+      margin:20
+    },
+    instructions: {
+      textAlign: 'center',
+      color: 'blue',
+      marginBottom: 10,
+    },
+  });
+
